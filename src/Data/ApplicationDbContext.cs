64cf36143d579ba78f7d5f9a665091eac079a324
3bdo-Yahya <A.Rhman.Yahya@gmail.com>
2026-03-02 18:01:01 +0200

    feat: Initialize new ASP.NET Core MVC project with Identity scaffolding.

---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cursus.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
