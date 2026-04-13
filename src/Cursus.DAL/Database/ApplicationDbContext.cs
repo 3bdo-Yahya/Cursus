3337b0ede9cfd3a8c65c701bb085b0f8c701536b
3bdo-yahya <a.rhman.yahya@gmail.com>
2026-04-13 17:42:33 +0200

    refactor: implement 3 tier on the database branch, and delete duplicates

--- src/Cursus.DAL/Database/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Cursus.DAL.Database
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
