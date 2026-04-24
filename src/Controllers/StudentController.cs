9bf0315174d7f9865835009c209857df156e0dc8
3bdo-yahya <a.rhman.yahya@gmail.com>
2026-04-24 20:36:06 +0300

    Refactor: Organize StudentController code structure for improved readability

--- src/Controllers/StudentController.cs
+++ src/Controllers/StudentController.cs
@@ -2,38 +2,41 @@
 using Microsoft.AspNetCore.Authorization;
 using Cursus.Models.ViewModels;
 
-[Authorize]
-public class StudentController : Controller
-{
-    public IActionResult Dashboard()
-    {
-        var model = new DashboardViewModel
-        {
-            StudentName = "Mohamed Hazem",
-            Department = "Computer Science",
-            CGPA = 3.2,
-            CreditsCompleted = 90,
-            TotalCreditsRequired = 120,
-            CoursesRemaining = 10,
-            AcademicStanding = AcademicStanding.Good,
-            ProjectedGraduation = "2027"
-        };
+namespace Cursus.Controllers  
+{  
+    [Authorize]  
+    public class StudentController : Controller  
+    {  
+        public IActionResult Dashboard()  
+        {  
+            var model = new DashboardViewModel  
+            {  
+                StudentName = "Mohamed Hazem",  
+                Department = "Computer Science",  
+                CGPA = 3.2,  
+                CreditsCompleted = 90,  
+                TotalCreditsRequired = 120,  
+                CoursesRemaining = 10,  
+                AcademicStanding = AcademicStanding.Good,  
+                ProjectedGraduation = "2027"  
+            };  
 
-        return View(model);
-    }
+            return View(model);  
+        }  
 
-    public IActionResult CourseMap()
-    {
-        return View();
-    }
+        public IActionResult CourseMap()  
+        {  
+            return View();  
+        }  
 
-    public IActionResult Progress()
-    {
-        return View();
-    }
+        public IActionResult Progress()  
+        {  
+            return View();  
+        }  
 
-    public IActionResult AiAdvisor()
-    {
-        return View();
+        public IActionResult AiAdvisor()  
+        {  
+            return View();  
+        }
     }
 }
