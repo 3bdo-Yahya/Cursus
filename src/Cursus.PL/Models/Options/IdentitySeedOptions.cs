97455914eb59e52b4c9ecb5086d71ebda721ff82
Mohamed-Hazem47 <mohamedhazem20040@gmail.com>
2026-05-13 20:39:59 +0300

    S2-005b: Move seed password to configuration

--- src/Cursus.PL/Models/Options/IdentitySeedOptions.cs
+++ src/Cursus.PL/Models/Options/IdentitySeedOptions.cs
@@ -2,6 +2,6 @@ namespace Cursus.PL.Models.Options;
 
 public sealed class IdentitySeedOptions
 {
-    public string AdminEmail { get; set; } = "admin@cursus.com";
+    public string AdminEmail { get; set; } = string.Empty;
     public string AdminPassword { get; set; } = string.Empty;
 }
