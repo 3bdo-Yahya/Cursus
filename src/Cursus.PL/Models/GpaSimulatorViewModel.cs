7d3ba471177debb99f0ea8363ece59a119934ee8
Mohamed-Hazem47 <mohamedhazem20040@gmail.com>
2026-06-20 00:05:49 +0300

    [S3-003c] Implement GPA Simulator Client Calculations in JS Write client-side Javascript calculations that update SGPA and CGPA in real-time as user changes dropdown grades.

---
+++ src/Cursus.PL/Models/GpaSimulatorViewModel.cs
@@ -0,0 +1,37 @@
+using Cursus.Domain.Enums;
+
+namespace Cursus.PL.Models;
+
+public class GpaSimulatorViewModel
+{
+    public required string StudentName { get; init; }
+    public required string Department { get; init; }
+    public int Year { get; init; }
+    public required string Semester { get; init; }
+
+    public double CurrentCgpa { get; init; }
+    public double LastSgpa { get; init; }
+    public required string AcademicStanding { get; init; }
+    public int CompletedCredits { get; init; }
+    public double CompletedQp { get; init; }
+
+    public required List<SimulatedCourseViewModel> CurrentCourses { get; init; }
+    public required List<ImprovableCourseViewModel> ImprovableCourses { get; init; }
+    public required Dictionary<string, double> GradeScale { get; init; }
+}
+
+public class SimulatedCourseViewModel
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public int Credits { get; init; }
+}
+
+public class ImprovableCourseViewModel
+{
+    public required string Id { get; init; }
+    public required string Name { get; init; }
+    public int Credits { get; init; }
+    public required string OriginalGrade { get; init; }
+    public double OriginalPoints { get; init; }
+}
